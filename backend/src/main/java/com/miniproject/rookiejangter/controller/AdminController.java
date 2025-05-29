e5c0ea4625fb26d615f5ab040db0948ed9b0e831
J-millar99 <wlwogus33@gmail.com>
2025-05-29 18:42:28 +0900

    feature/#22: Controller Add

--- backend/src/main/java/com/miniproject/rookiejangter/controller/AdminController.java
+++ backend/src/main/java/com/miniproject/rookiejangter/controller/AdminController.java
@@ -1,5 +1,21 @@
 package com.miniproject.rookiejangter.controller;
 
+import org.springframework.http.HttpStatus;
+// 사용자 관리 컨트롤러
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
-    
+
+    // 관리자의 유저 목록 조회
+    @GetMapping
+    public ResponseEntity<List<UserDTO.Response>> getAllUsers() {
+        
+    }
+
+    // 관리자의 특정 유저 조회
+    @GetMapping("users/{id}")
+    public ResponseEntity<UserDTO.Response> getUserById(@PathVariable Long id) {
+
+    }
 }
