471d5a6c89b1c0719dc330a9abd3bfb1059fc0be
J-millar99 <wlwogus33@gmail.com>
2025-05-30 23:18:09 +0900

    Controller

--- backend/src/main/java/com/miniproject/rookiejangter/controller/AuthController.java
+++ backend/src/main/java/com/miniproject/rookiejangter/controller/AuthController.java
@@ -1,6 +1,15 @@
 package com.miniproject.rookiejangter.controller;
 
+import com.miniproject.rookiejangter.controller.dto.UserDTO;
+import com.miniproject.rookiejangter.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -11,11 +20,19 @@ public class AuthController {
     // 회원가입
     @PostMapping("/signup")
     public ResponseEntity<UserDTO.Response> registerUser(@Valid @RequestBody UserDTO.Request request) {
+        UserDTO.Response user = userService.createUser(request);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<UserDTO.LoginResponse> loginUser(@Valid @RequestBody UserDTO.LoginRequest request) {
+    public ResponseEntity<UserDTO.Response> loginUser(@RequestBody UserDTO.LoginRequest request) {
+        boolean isAuthenticated = userService.authenticate(request);
+        if (isAuthenticated) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
     }
 }
 
--- backend/src/main/java/com/miniproject/rookiejangter/controller/UserController.java
+++ backend/src/main/java/com/miniproject/rookiejangter/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.miniproject.rookiejangter.controller;
 
+import com.miniproject.rookiejangter.controller.dto.UserDTO;
+import com.miniproject.rookiejangter.service.ProductService;
+import com.miniproject.rookiejangter.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,16 +13,42 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    private final ProductService productService;
 
     // 유저 상세 정보 조회
     @GetMapping("/{id}/profile")
     public ResponseEntity<UserDTO.Response> getUserById(@PathVariable Long id) {
-
+        UserDTO.Response users = userService.getUserById(id);
+        return ResponseEntity.ok(users);
     }
 
     // 유저 상세 정보 수정
     @PutMapping("/{id}/profile")
-    public ResponseEntity<UserDTO.Response> getUserById(@PathVariable Long id) {
-
+    public ResponseEntity<UserDTO.Response> updateUserById(@PathVariable Long id, @Valid @RequestBody UserDTO.UpdateRequest request) {
+        UserDTO.Response users = userService.updateUser(id, request);
+        return ResponseEntity.ok(users);
     }
+
+    // 유저 정보 삭제
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 유저 상품 등록
+    @PostMapping("/{user_id}/products")
+
+    // 유저가 등록한 모든 상품 조회
+    @GetMapping("/{user_id}/products")
+
+    // 유저가 등록한 특정 상품 조회
+    @GetMapping("/{user_id}/products/{product_id}")
+
+    // 유저가 등록한 상품 수정
+    @PutMapping("/{user}/products/{product_id}")
+
+    // 유저가 등록한 상품 삭제
+    @DeleteMapping("/{user}/products/{product_id}")
 }
