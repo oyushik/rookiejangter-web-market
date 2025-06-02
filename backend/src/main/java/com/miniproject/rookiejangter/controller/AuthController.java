787570c2a3c7491e9effb053a07e4d43df5c9052
J-millar99 <wlwogus33@gmail.com>
2025-06-02 10:35:23 +0900

    프록시 설정

--- backend/src/main/java/com/miniproject/rookiejangter/controller/AuthController.java
+++ backend/src/main/java/com/miniproject/rookiejangter/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
