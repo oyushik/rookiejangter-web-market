a20a0ba84ed7bbe9152e5a39f4ec2a61b498bf2f
oyushik <craven_sawdust.9i@icloud.com>
2025-06-07 11:42:37 +0900

    feat#96: 거래 예약 -> 판매자 알림 구현

--- backend/src/main/java/com/miniproject/rookiejangter/controller/ReservationController.java
+++ backend/src/main/java/com/miniproject/rookiejangter/controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reservation")
+@RequestMapping("/api/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
 
