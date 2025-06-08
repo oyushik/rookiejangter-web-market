38c07c0e6e802682356458b9e2d64d4b42329161
J-millar99 <wlwogus33@gmail.com>
2025-06-08 15:47:45 +0900

    redis 충돌 해결

--- backend/src/main/java/com/miniproject/rookiejangter/config/RedisConfig.java
+++ backend/src/main/java/com/miniproject/rookiejangter/config/RedisConfig.java
@@ -3,25 +3,19 @@ package com.miniproject.rookiejangter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
-    }
+    // @Bean
+    // public RedisConnectionFactory redisConnectionFactory() {
+    //     return new LettuceConnectionFactory("localhost", 6379);
+    // }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
